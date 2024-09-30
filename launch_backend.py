0426fa2310a701418a18d3717ae30e209890298f
meg <90473723+meg-huggingface@users.noreply.github.com>
2024-09-30 13:09:45 -0700

    Update launch_backend.py

--- launch_backend.py
+++ launch_backend.py
@@ -3,12 +3,10 @@ import os
 
 TOKEN = os.environ.get("DEBUG")
 
-
 API = HfApi(token=TOKEN)
 def start_space():
-    API.pause_space("meg/launching-test-3")
-    API.restart_space("meg/launching-test-3")
-
+    API.pause_space("EnergyStarAI/backend_test")
+    API.restart_space("EnergyStarAI/backend_test")
 
 if __name__ == "__main__":
   start_space()
